8ea406c11f8c4ead7dbed33d02e5be5462c1d263
Zilove <niuulu@126.com>
2024-10-18 23:17:41 +0800

    Initial commit.

---
+++ LG/B2016.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    double a;
+    scanf("%lf",&a);
+    long long b = (long long )a;
+    printf("%lld", b);
+    return 0;
+}
---
+++ LG/B2019.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    int a, b;
+    bool c, d;
+    scanf("%d", &a);
+    c = (a != 0);
+    d = c;
+    printf("%d", d);
+    return 0;
+}
