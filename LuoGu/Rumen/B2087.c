455ce7549c248594f0f3170653339dba19749e79
Zilove <Niuulu@126.com>
2024-11-04 23:30:06 +0800

    11.4

--- LuoGu/Rumen/B2087.c
+++ LuoGu/Rumen/B2087.c
@@ -1,18 +1,39 @@
+ /*                              
+                   ┌─┐       ┌─┐
+                ┌──┘ ┴───────┘ ┴──┐
+                │                 │
+                │       ───       │
+                │  ─┬┘       └┬─  │牛子陆错母娘。真舒服。
+                │                 │
+                │       ─┴─       │
+                │                 │
+                └───┐         ┌───┘
+                    │         │
+                    │         │
+                    │         │
+                    │         └──────────────┐
+                    │                        │
+                    │                        ├─┐
+                    │                        ┌─┘
+                    │                        │
+                    └─┐  ┐  ┌───────┬──┐  ┌──┘
+                      │ ─┤ ─┤       │ ─┤ ─┤
+                      └──┴──┘       └──┴──┘
+*/
 #include <stdio.h>
 
-int main() {
-    int n, m, count = 0;
+int main(){
+    int n, m, count =0;
     scanf("%d", &n);
     int arr[n];
-    for (int i = 0; i < n; i++) {
+    for(int i = 0;i<n;i++){
         scanf("%d", &arr[i]);
     }
     scanf("%d", &m);
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == m) {
+    for(int i = 0;i<n;i++){
+        if(m == arr[i]){
             count++;
         }
     }
-    printf("%d\n", count);
-    return 0;
+    printf("%d", count);
 }
